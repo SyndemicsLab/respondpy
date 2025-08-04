dbe3a3983688b20f794068b977432c91a8468676
Matthew Carroll <28577806+MJC598@users.noreply.github.com>
2025-08-04 14:37:11 -0400

    removing the unnecessary respond py file

--- src/respondpy/__init__.py
+++ src/respondpy/__init__.py
@@ -12,10 +12,16 @@
 
 from __future__ import annotations
 
-from . import respond
+from ._core import (  # pylint: disable=import-error,no-name-in-module
+    CostStamp,
+    CreationStatus, HistoryStamp, LogType, Markov, ResultSets, Totals, UtilityType, behavior, calculate_life_years, calculate_perspectives, calculate_total_costs, create_file_logger, discount, discount_cost_stamp, intervention,
+    kDebug, kError, kExists, kInfo, kMin, kMult, kNotCreated, kSuccess, kWarn, log_debug, log_error, log_info, log_warning, migration, mortality, overdose, stamp_costs, stamp_costs_over_time, stamp_utilities, stamp_utilities_over_time
+)
 
-__all__ = ['CostStamp', 'CreationStatus', 'HistoryStamp', 'LogType', 'Markov', 'ResultSets', 'Totals', 'UtilityType', 'behavior', 'calculate_life_years', 'calculate_perspectives', 'calculate_total_costs', 'create_file_logger', 'discount', 'discount_cost_stamp', 'intervention',
-           'kDebug', 'kError', 'kExists', 'kInfo', 'kMin', 'kMult', 'kNotCreated', 'kSuccess', 'kWarn', 'log_debug', 'log_error', 'log_info', 'log_warning', 'migration', 'mortality', 'overdose', 'stamp_costs', 'stamp_costs_over_time', 'stamp_utilities', 'stamp_utilities_over_time']
+__all__ = [
+    'CostStamp', 'CreationStatus', 'HistoryStamp', 'LogType', 'Markov', 'ResultSets', 'Totals', 'UtilityType', 'behavior', 'calculate_life_years', 'calculate_perspectives', 'calculate_total_costs', 'create_file_logger', 'discount', 'discount_cost_stamp', 'intervention',
+    'kDebug', 'kError', 'kExists', 'kInfo', 'kMin', 'kMult', 'kNotCreated', 'kSuccess', 'kWarn', 'log_debug', 'log_error', 'log_info', 'log_warning', 'migration', 'mortality', 'overdose', 'stamp_costs', 'stamp_costs_over_time', 'stamp_utilities', 'stamp_utilities_over_time'
+]
 
 
 def __dir__() -> list[str]:
