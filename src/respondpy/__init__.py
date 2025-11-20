f6b413574bc2be71839806955525fad74bd8d9fb
Matthew Carroll <28577806+MJC598@users.noreply.github.com>
2025-11-20 13:59:20 -0500

    Adding functions to get numpy from sqlite

--- src/respondpy/__init__.py
+++ src/respondpy/__init__.py
@@ -4,7 +4,7 @@
 # Created Date: 2025-08-04                                                     #
 # Author: Matthew Carroll                                                      #
 # -----                                                                        #
-# Last Modified: 2025-08-04                                                    #
+# Last Modified: 2025-11-20                                                    #
 # Modified By: Matthew Carroll                                                 #
 # -----                                                                        #
 # Copyright (c) 2025 Syndemics Lab at Boston Medical Center                    #
@@ -18,9 +18,13 @@ from ._core import (  # pylint: disable=import-error,no-name-in-module
     kDebug, kError, kExists, kInfo, kMin, kMult, kNotCreated, kSuccess, kWarn, log_debug, log_error, log_info, log_warning, migration, mortality, overdose, stamp_costs, stamp_costs_over_time, stamp_utilities, stamp_utilities_over_time
 )
 
+from .sqlite_eigen import (
+    get_init_cohort_from_db, get_population_change_from_db, get_intervention_transitions_from_db, get_behavior_transitions_from_db, get_overdose_from_db, get_fatal_overdose_from_db, get_background_mortality_from_db, get_smr_from_db
+)
+
 __all__ = [
     'CostStamp', 'CreationStatus', 'HistoryStamp', 'LogType', 'Markov', 'ResultSets', 'Totals', 'UtilityType', 'behavior', 'calculate_life_years', 'calculate_perspectives', 'calculate_total_costs', 'create_file_logger', 'discount', 'discount_cost_stamp', 'intervention',
-    'kDebug', 'kError', 'kExists', 'kInfo', 'kMin', 'kMult', 'kNotCreated', 'kSuccess', 'kWarn', 'log_debug', 'log_error', 'log_info', 'log_warning', 'migration', 'mortality', 'overdose', 'stamp_costs', 'stamp_costs_over_time', 'stamp_utilities', 'stamp_utilities_over_time'
+    'kDebug', 'kError', 'kExists', 'kInfo', 'kMin', 'kMult', 'kNotCreated', 'kSuccess', 'kWarn', 'log_debug', 'log_error', 'log_info', 'log_warning', 'migration', 'mortality', 'overdose', 'stamp_costs', 'stamp_costs_over_time', 'stamp_utilities', 'stamp_utilities_over_time', 'get_init_cohort_from_db', 'get_population_change_from_db', 'get_intervention_transitions_from_db', 'get_behavior_transitions_from_db', 'get_overdose_from_db', 'get_fatal_overdose_from_db', 'get_background_mortality_from_db', 'get_smr_from_db'
 ]
 
 
