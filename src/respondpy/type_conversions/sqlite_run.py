1026f40f3726af75dae255a1160a65df5d130b0c
Matthew Carroll <28577806+MJC598@users.noreply.github.com>
2025-11-26 12:44:29 -0500

    updating the build procedure

---
+++ src/respondpy/type_conversions/sqlite_run.py
@@ -0,0 +1,11 @@
+################################################################################
+# File: sqlite_run.py                                                          #
+# Project: respondpy                                                           #
+# Created Date: 2025-11-25                                                     #
+# Author: Matthew Carroll                                                      #
+# -----                                                                        #
+# Last Modified: 2025-11-25                                                    #
+# Modified By: Matthew Carroll                                                 #
+# -----                                                                        #
+# Copyright (c) 2025 Syndemics Lab at Boston Medical Center                    #
+################################################################################
