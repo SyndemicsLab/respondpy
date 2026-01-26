9263ae59e774aede6d4be98375dff702df4b58e1
Matthew Carroll <28577806+MJC598@users.noreply.github.com>
2026-01-26 10:49:01 -0500

    Improving test coverage

--- tests/test_writing.py
+++ tests/test_writing.py
@@ -4,7 +4,7 @@
 # Created Date: 2026-01-16                                                     #
 # Author: Matthew Carroll                                                      #
 # -----                                                                        #
-# Last Modified: 2026-01-21                                                    #
+# Last Modified: 2026-01-26                                                    #
 # Modified By: Matthew Carroll                                                 #
 # -----                                                                        #
 # Copyright (c) 2026 Syndemics Lab at Boston Medical Center                    #
@@ -37,6 +37,14 @@ def setup(tmp_path_factory, db_schema, db_cohort_insert, db_intervention_insert,
     conn.commit()
     conn.close()
     return mem_str
+
+
+def delete(db_file, table_name):
+    conn = sqlite3.connect(db_file)
+    cursor = conn.cursor()
+    cursor.execute(f"DELETE FROM {table_name};")
+    conn.commit()
+    conn.close()
 
 
 class TestWriting:
@@ -171,3 +179,131 @@ class TestWriting:
         with pytest.raises(FileNotFoundError, match="The specified database could not be found:"):
             insert_parameter([], "",
                              Parameter.OVERDOSE_FATALITY_PROBABILITY)
+
+
+class TestInvalidWriting:
+    """Class to test the invalid writing attempts
+    """
+
+    def _count_rows(self, db_file: str | Path, table: str) -> int:
+        con = sqlite3.connect(db_file)
+        cur = con.cursor()
+        stmt = f"SELECT COUNT(*) FROM {table}"
+        cur.execute(stmt)
+        result = cur.fetchone()[0]
+        con.close()
+        return result
+
+    def test_insert_initial_cohort(self, setup):
+        db_file = setup
+        delete(db_file, "initial_population")
+        df = pl.DataFrame({
+            "sample": [1, 1, 1, 1],
+            "intervention": [1, 1, 2, 2],
+            "behavior": [1, 2, 1, 2]
+        })
+
+        insert_parameter(df.rows(),
+                         db_file, Parameter.INITIAL_COHORT)
+
+        assert self._count_rows(db_file, "initial_population") == 0
+
+    def test_insert_migrating_cohort(self, setup):
+        db_file = setup
+        delete(db_file, "population_change")
+        df = pl.DataFrame({
+            "sample": [1, 1, 1, 1],
+            "intervention": [1, 1, 2, 2],
+            "behavior": [1, 2, 1, 2],
+            "time": [1, 1, 1, 1]
+        })
+        insert_parameter(df.rows(),
+                         db_file, Parameter.MIGRATION_COHORT)
+
+        assert self._count_rows(db_file, "population_change") == 0
+
+    def test_insert_intervention_transition(self, setup):
+        db_file = setup
+        delete(db_file, "intervention_transition")
+        df = pl.DataFrame({
+            "sample": [1, 1, 1, 1, 1, 1, 1, 1],
+            "behavior": [1, 2, 1, 2, 1, 2, 1, 2],
+            "time": [1, 1, 1, 1, 1, 1, 1, 1],
+            "initial_intervention": [1, 1, 2, 2, 1, 1, 2, 2],
+            "new_intervention": [1, 1, 1, 1, 2, 2, 2, 2]
+        })
+        insert_parameter(df.rows(),
+                         db_file, Parameter.INTERVENTION_TRANSITION_PROBABILITY)
+
+        assert self._count_rows(
+            db_file, "intervention_transition") == 0
+
+    def test_insert_behavior_transition(self, setup):
+        db_file = setup
+        delete(db_file, "behavior_transition")
+        df = pl.DataFrame({
+            "sample": [1, 1, 1, 1, 1, 1, 1, 1],
+            "intervention": [1, 2, 1, 2, 1, 2, 1, 2],
+            "time": [1, 1, 1, 1, 1, 1, 1, 1],
+            "initial_behavior": [1, 1, 2, 2, 1, 1, 2, 2],
+            "new_behavior": [1, 1, 1, 1, 2, 2, 2, 2]
+        })
+        insert_parameter(df.rows(),
+                         db_file, Parameter.BEHAVIOR_TRANSITION_PROBABILITY)
+
+        assert self._count_rows(
+            db_file, "behavior_transition") == 0
+
+    def test_insert_smr(self, setup):
+        db_file = setup
+        delete(db_file, "smr")
+        df = pl.DataFrame({
+            "sample": [1, 1, 1, 1],
+            "intervention": [1, 1, 2, 2],
+            "behavior": [1, 2, 1, 2],
+            "time": [1, 1, 1, 1]
+        })
+        insert_parameter(df.rows(),
+                         db_file, Parameter.STANDARD_MORTALITY_RATIO)
+
+        assert self._count_rows(db_file, "smr") == 0
+
+    def test_insert_background_mortality(self, setup):
+        db_file = setup
+        delete(db_file, "background_mortality")
+        df = pl.DataFrame({
+            "sample": [1],
+            "time": [1]
+        })
+        insert_parameter(df.rows(),
+                         db_file, Parameter.BACKGROUND_DEATH_PROBABILITY)
+
+        assert self._count_rows(db_file, "background_mortality") == 0
+
+    def test_insert_overdose(self, setup):
+        db_file = setup
+        delete(db_file, "overdose")
+        df = pl.DataFrame({
+            "intervention": [1, 1, 2, 2],
+            "sample": [1, 1, 1, 1],
+            "behavior": [1, 2, 1, 2],
+            "time": [1, 1, 1, 1]
+        })
+        insert_parameter(df.rows(),
+                         db_file, Parameter.OVERDOSE_PROBABILITY)
+
+        assert self._count_rows(db_file, "overdose") == 0
+
+    def test_insert_fatal_overdose(self, setup):
+        db_file = setup
+        delete(db_file, "overdose_fatality")
+        df = pl.DataFrame({
+            "sample": [1, 1, 1, 1],
+            "intervention": [1, 1, 2, 2],
+            "behavior": [1, 2, 1, 2],
+            "time": [1, 1, 1, 1]
+        })
+        insert_parameter(df.rows(),
+                         db_file, Parameter.OVERDOSE_FATALITY_PROBABILITY)
+
+        assert self._count_rows(db_file, "overdose_fatality") == 0
